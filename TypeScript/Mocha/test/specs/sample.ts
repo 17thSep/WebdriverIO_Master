d4ba9182fd78e6326d6b8297b5e904a49968345d
Aravinth Ponnusamy <AP@aravinths-mini.home>
2021-02-12 19:19:12 +0000

    Mocha final

--- TypeScript/Mocha/test/specs/sample.ts
+++ TypeScript/Mocha/test/specs/sample.ts
@@ -1,20 +1,20 @@
-// /* ***                              Hint 1                                   *** 
-// Try entering the spelling for describe incorrectly, TypeScript wont allow you */
-// describe("Sample Test set", () => {
-//   it("Test case", () => {
-//     /* ***                          Hint 2                                   *** 
-//     Mouse hover browser you will the assosicated libraries */
-//     browser.navigateTo("https://the-internet.herokuapp.com/login");
+/* ***                              Hint 1                                   *** 
+Try entering the spelling for describe incorrectly, TypeScript wont allow you */
+describe("Sample Test set", () => {
+  it("Test case", () => {
+    /* ***                          Hint 2                                   *** 
+    Mouse hover browser you will the assosicated libraries */
+    browser.navigateTo("https://the-internet.herokuapp.com/login");
 
-//     /* ***                          Hint 3                                   *** 
-//     Enter $ alone you will the suggestion */
-//     $("//*[@id='username']").click();
+    /* ***                          Hint 3                                   *** 
+    Enter $ alone you will the suggestion */
+    $("//*[@id='username']").click();
 
-//     /* ***                          Hint 4                                   *** 
-//     Enter $("//*[@id='username']") and enter . all the assosicated functions will be shown */
-//     $("//*[@id='username']").setValue("tomsmith");
+    /* ***                          Hint 4                                   *** 
+    Enter $("//*[@id='username']") and enter . all the assosicated functions will be shown */
+    $("//*[@id='username']").setValue("tomsmith");
 
-//     $("#password").setValue("SuperSecretPassword!");
-//     $('button[type="submit"]').click();
-//   });
-// });
+    $("#password").setValue("SuperSecretPassword!");
+    $('button[type="submit"]').click();
+  });
+});
