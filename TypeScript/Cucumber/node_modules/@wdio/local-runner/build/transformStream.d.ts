61fd6b997937d56be5cb6726c29b0301a5a260eb
17thSep.net <75314655+17thSep@users.noreply.github.com>
2021-02-17 18:36:00 +0000

    Delete TypeScript/Cucumber/node_modules directory

--- TypeScript/Cucumber/node_modules/@wdio/local-runner/build/transformStream.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { Transform, TransformCallback } from 'stream';
-export default class RunnerTransformStream extends Transform {
-    cid: string;
-    constructor(cid: string);
-    _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void;
-    _final(callback: (error?: Error | null) => void): void;
-}
-//# sourceMappingURL=transformStream.d.ts.map
